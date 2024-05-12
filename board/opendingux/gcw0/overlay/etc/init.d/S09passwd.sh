a2eddee5467b14e92e5370efc2322c16001b8051
game-de-it <game.de.it.0@gmail.com>
2024-05-13 00:26:56 +0900

    plumOS-jz47xx-beta-P2

---
+++ board/opendingux/gcw0/overlay/etc/init.d/S09passwd.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+[ -z "$1" ] || [ "x$1" = "xstart" ] || exit 0
+
+if [ ! -f /usr/local/etc/shadow ]; then
+	echo 'od:*:::::::' > /usr/local/etc/shadow
+	echo 'root:*:::::::' > /usr/local/etc/shadow
+	chmod 600 /usr/local/etc/shadow
+fi
